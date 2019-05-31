80e98a4a11f6bb63691dabbc8f9dcf3934af3515
jack <jackhuang1205@gmail.com>
2019-05-31 23:53:40 +0800

    Get message from mqtt

--- python/send2Arduino.py
+++ python/send2Arduino.py
@@ -2,6 +2,7 @@ import paho.mqtt.publish as publish
 import paho.mqtt.client as mqtt
 import json
 import time
+import threading
 
 # publish a message then disconnect.
 host = "35.233.174.129"
@@ -12,6 +13,20 @@ def sendMessage(x, y, theta, mode):
     dic = {"x" : x, "y" : y, "theta" : theta, "mode" : mode}
     payload = json.dumps(dic)
     publish.single(topic, payload, qos=1, hostname=host)
+
+
+def testM(client, userdata, message):
+    print(str(message.payload.decode("utf-8")))
+
+
+class ListenMqtt():
+
+    def __init__(self, trigger, host = "35.233.174.129", topic = "home/test1"):
+        self.client = mqtt.Client(host)
+        self.client.on_message = trigger 
+        self.client.connect(host)
+        self.client.loop_start()
+        self.client.subscribe(topic)
 
 
 
